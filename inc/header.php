91207f5e4169eb1c759749279c91006c1110eba4
Axel Barbé--Husson <47109555+AxelVitch@users.noreply.github.com>
2020-01-31 09:59:26 +0100

    Commit Project Instancing project

---
+++ inc/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Corail Bukake</title>
+</head>
+<body>
+
+<header>
+    <nav>
+
+    </nav>
+</header>
