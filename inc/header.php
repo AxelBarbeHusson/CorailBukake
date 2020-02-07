0200522f325317f381d01bc69aa8db8dcfea53d9
Bigo0612 <57392452+Bigo0612@users.noreply.github.com>
2020-02-07 01:28:27 +0100

    add image corailbukake.jpg and loupe.png start navigation include header.php in index.php

--- inc/header.php
+++ inc/header.php
@@ -2,13 +2,28 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../asset/css/styles.css">
     <title>Corail Bukake</title>
 </head>
 <body>
 
 <header>
-    <nav>
 
-    </nav>
+    <nav class="navbar">
+        <div class="container">
+            <a class="acceuil" href="index.php"><img src="../asset/img/corailbukake.jpg" alt="logo Corail Bakake"></a>
+            <form class="navbar-recherche">
+                <div class="groupe-recherche">
+                    <input type="text" class="cherche" placeholder="recherche de cv">
+                    <div class="recherche">
+                        <button class="btn" type="button">
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+
+
 </header>
